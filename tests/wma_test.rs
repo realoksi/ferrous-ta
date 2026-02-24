97e827e9e579b69566873ec20f983d3f756a24d5
realoksi <realoksi@pm.me>
2026-02-24 09:56:06 -0500

    use rust_decimal instead of f64 prim

--- tests/wma_test.rs
+++ tests/wma_test.rs
@@ -1,6 +1,7 @@
 use ferrous_ta::*;
 
 use csv::ReaderBuilder;
+use rust_decimal::{Decimal, RoundingStrategy};
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
@@ -8,19 +9,23 @@ struct Record {
     #[serde(rename = "t")]
     open_time: i64,
     #[serde(rename = "o")]
-    open_price: f64,
+    open_price: Decimal,
     #[serde(rename = "h")]
-    high_price: f64,
+    high_price: Decimal,
     #[serde(rename = "l")]
-    low_price: f64,
+    low_price: Decimal,
     #[serde(rename = "c")]
-    close_price: f64,
+    close_price: Decimal,
     #[serde(rename = "v")]
-    volume: f64,
-    sma: Option<f64>,
-    vwap: Option<f64>,
-    kama: Option<f64>,
-    wma: Option<f64>,
+    volume: Decimal,
+    #[serde(with = "rust_decimal::serde::str_option")]
+    sma: Option<Decimal>,
+    #[serde(with = "rust_decimal::serde::str_option")]
+    vwap: Option<Decimal>,
+    #[serde(with = "rust_decimal::serde::str_option")]
+    kama: Option<Decimal>,
+    #[serde(with = "rust_decimal::serde::str_option")]
+    wma: Option<Decimal>,
 }
 
 #[test]
@@ -38,7 +43,10 @@ fn test_wma_5() {
         let record: Record = record.unwrap();
 
         if let Some(value) = wma.step(record.close_price) {
-            assert_eq!(record.wma.unwrap(), (value * 100.0).round() / 100.0);
+            assert_eq!(
+                record.wma.unwrap(),
+                value.round_dp_with_strategy(2, RoundingStrategy::MidpointAwayFromZero)
+            );
         }
     }
 }

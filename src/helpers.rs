a94d03df6ca2b1c98ea36b7afc62b924075f59ac
realoksi <realoksi@pm.me>
2026-03-05 08:52:39 -0500

    removed a few unused helper methods

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,20 +1,5 @@
 use core::ops::{Add, Sub};
 
-#[inline]
-pub(crate) fn partial_max2<T: PartialOrd>(a: T, b: T) -> T {
-    if a > b { a } else { b }
-}
-
-#[inline]
-pub(crate) fn partial_min2<T: PartialOrd>(a: T, b: T) -> T {
-    if a > b { b } else { a }
-}
-
-#[inline]
-pub(crate) fn get_tr<T: Copy + PartialOrd + Sub<Output = T>>(high: T, low: T, prev_close: T) -> T {
-    partial_max2(high, prev_close) - partial_min2(low, prev_close)
-}
-
 pub struct SlidingWindow<T, const N: usize> {
     buf: [T; N],
     idx: usize,
